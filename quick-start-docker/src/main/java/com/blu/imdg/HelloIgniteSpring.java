d452811396b6f3d686121b0b4d018140b43b12fb
shamim <srecon@yandex.ru>
2024-02-13 10:59:07 +0300

    Modify benchmark script

--- quick-start-docker/src/main/java/com/blu/imdg/HelloIgniteSpring.java
+++ quick-start-docker/src/main/java/com/blu/imdg/HelloIgniteSpring.java
@@ -4,13 +4,18 @@ import com.blu.dto.Person;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Running the example with Java 11 or later
  * Use the following JVM options to run on JVM 17 or later
  * java --add-opens=jdk.management/com.sun.management.internal=ALL-UNNAMED --add-opens=java.base/jdk.internal.misc=ALL-UNNAMED --add-opens=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.management/com.sun.jmx.mbeanserver=ALL-UNNAMED --add-opens=jdk.internal.jvmstat/sun.jvmstat.monitor=ALL-UNNAMED --add-opens=java.base/sun.reflect.generics.reflectiveObjects=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.nio=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED -jar ./target/HelloIgniteSpring-runnable.jar
  * */
 public class HelloIgniteSpring {
+    private static final Logger logger = LoggerFactory.getLogger(HelloThinClient.class);
     public static void main(String[] args) {
+        logger.info("Ignite Spring configuration example as a client working over TCP socket.");
         // Start Ignite cluster
         Ignite ignite = Ignition.start("default-config.xml");
 
@@ -26,7 +31,12 @@ public class HelloIgniteSpring {
         cache.put(3, p3);
         cache.put(4, p4);
 
-        System.out.println("Enter crtl-x to quite the application!!!");
+        // get scott age
+        int val = cache.get(3).getAge();
+
+        logger.info("Print value: {}", val);
+
+        logger.info("Enter crtl-x to quite the application!!!");
 
     }
 }
